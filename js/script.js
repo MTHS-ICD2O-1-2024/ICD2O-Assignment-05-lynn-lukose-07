7ada798ce71b820992e0357d3ecb8655ce2b0696
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-06-11 02:56:58 +0000

    linter fix

--- js/script.js
+++ js/script.js
@@ -4,21 +4,21 @@
 // Created on: May 2025
 // This file contains the JS functions for index.html
 
-"use strict"
+'use strict'
 // this function reverses words using a for loop
-function reverseString() {
-  let reversed = "" // Initialize an empty string to store the reversed result
+function reverseString () {
+  let reversed = '' // Initialize an empty string to store the reversed result
 
   // Input
-let originalString = document.getElementById("inputString").value
+    let originalString = document.getElementById('inputString').value
 
   // Use a for loop to reverse the string
-for (let counter = originalString.length - 1; counter >= 0; counter--) {
+    for (let counter = originalString.length - 1; counter >= 0; counter--) {
     const char = originalString.charAt(counter)
     reversed = reversed + char // Add each character to the reversed string
-}
+    }
 
   // output"
-document.getElementById("result").innerHTML =
-    "<p>The reversed string is " + reversed + ".</p>"
+    document.getElementById('result').innerHTML =
+    '<p>The reversed string is ' + reversed + '.</p>'
 }
